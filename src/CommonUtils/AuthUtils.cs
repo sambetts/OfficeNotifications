d2351491dc9ca52b0b12a08b3cab1b43fb422c39
Sam Betts <sambetts@microsoft.com>
2022-08-12 18:46:25 +0200

    Initial
    .
    
    .
    
    templates

---
+++ src/CommonUtils/AuthUtils.cs
@@ -0,0 +1,24 @@
+﻿using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CommonUtils
+{
+    public class AuthUtils
+    {
+        private static Dictionary<string, X509Certificate2> _cachedCerts = new ();
+        public static async Task<X509Certificate2> RetrieveKeyVaultCertificate(string name, string tenantId, string clientId, string clientSecret, string keyVaultUrl)
+        {
+            if (!_cachedCerts.ContainsKey(name))
+            {
+                var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new ClientSecretCredential(tenantId, clientId, clientSecret));
+
+                var secret = await client.GetSecretAsync(name);
+
+                _cachedCerts.Add(name, new X509Certificate2(Convert.FromBase64String(secret.Value.Value)));
+            }
+            return _cachedCerts[name];
+
+        }
+    }
+}
---
+++ src/OfficeNotifications.Engine/Models/Config.cs
@@ -0,0 +1,47 @@
+﻿using CommonUtils;
+using CommonUtils.Config;
+using OfficeNotifications.Engine.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OfficeNotifications.Engine
+{
+
+    public class Config : AzureAdWithTelemetryConfig
+    {
+        public Config(Microsoft.Extensions.Configuration.IConfiguration config) : base(config) { }
+
+        [ConfigValue] public string BlobContainerName { get; set; } = string.Empty;
+
+        [ConfigValue] public string SharePointSiteId { get; set; } = string.Empty;
+        [ConfigValue(true)] public string KeyVaultUrl { get; set; } = string.Empty;
+        [ConfigValue] public string WebhookUrlOverride { get; set; } = string.Empty;
+
+        public string ServiceBusQueueName => "graphupdates";
+        public string AzureTableActivity => "activity";
+        public string AzureTableLocks => "locks";
+        public string AzureTablePropertyBag => "propertybag";
+
+        [ConfigSection("ConnectionStrings")] public ConnectionStrings ConnectionStrings { get; set; } = null!;
+
+    }
+
+    public class ConnectionStrings : PropertyBoundConfig
+    {
+        public ConnectionStrings(Microsoft.Extensions.Configuration.IConfigurationSection config) : base(config)
+        {
+        }
+
+        [ConfigValue]
+        public string Storage { get; set; } = string.Empty;
+
+        [ConfigValue]
+        public string ServiceBus { get; set; } = string.Empty;
+
+        [ConfigValue]
+        public string Redis { get; set; } = string.Empty;
+    }
+}
